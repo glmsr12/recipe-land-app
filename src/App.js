c90aa74dd4563ebfba5d45f43545a9bedc971378
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-15 09:59:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Favorite from './components/Favorite/Favorite';
+import HomePage from './components/HomePage/HomePage';
+import RandomRecipes from './components/RandomRecipes/RandomRecipes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+
+          <Route exact path="/favorites" element={<Favorite />} />
+
+          <Route exact path="/randomrecipes" element={<RandomRecipes />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
